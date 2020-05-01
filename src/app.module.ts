4d5e761212c8d10165fe1b8aaed3c4c8663fca33
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-05-01 15:21:00 +0200

    Archi: Setup MongoDB & GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_URL,
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
