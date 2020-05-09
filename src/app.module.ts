5dade69c5d4027f457f19aca100be77f1f8f8f59
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-05-09 18:53:32 +0200

    Back: Email module completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,42 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailerModule } from '@nestjs-modules/mailer';
+
 import { MealModule } from './meal/meal.module';
-import { Meal } from './meal/meal.entity';
 import { AuthModule } from './auth/auth.module';
+
+import { Meal } from './meal/meal.entity';
 import { User } from './auth/user.entity';
+
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mongodb',
       url: 'mongodb://localhost/side_meals',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Meal, User]
+      entities: [Meal, User],
     }),
     GraphQLModule.forRoot({ autoSchemaFile: true }),
+    MailerModule.forRoot({
+      transport: `smtps://${process.env.MAILER_DOMAIN}:${process.env.MAILER_PASSWORD}@${process.env.SMTP_PROVIDER}`,
+      defaults: {
+        from:'"nest-modules" <modules@nestjs.com>',
+      },
+      preview: false,
+      template: {
+        dir: process.cwd() + '/src/templates',
+        adapter: new EjsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     MealModule,
     AuthModule,
   ],
